c9199ef0f1d2404f8fee6b42c23438ae02045ff4
meamod <james@fe0.uk>
2015-02-20 08:56:22 +0000

    First Commit

---
+++ FilterMD5/Module1.vb
@@ -0,0 +1,41 @@
+﻿Imports Microsoft.VisualBasic.FileIO
+Imports System.Text
+Imports System.IO
+Imports System.Runtime.Serialization
+Imports System.Runtime.Serialization.Formatters.Binary
+
+Module Module1
+    Dim Key As String = "!hX7W#i]3:nh<Fj>+{[,.MvIRF=Kk!+Sm8DBFEqi@9dU/\iLS}UR;C)]LZ;fm?>"
+    Dim i As Integer = 0
+    Dim stfi As ArrayList = New ArrayList
+    Sub Main()
+        Dim filename As String = "filter.csv"
+        Dim fields As String()
+        Dim delimiter As String = ","
+
+        Using parser As New TextFieldParser(filename)
+            parser.SetDelimiters(delimiter)
+            While Not parser.EndOfData
+                ' Read in the fields for the current line
+                fields = parser.ReadFields()
+
+                ' Add code here to use data in fields variable.
+                Dim bf As New BinaryFormatter()
+                For Each Word As String In fields
+                    stfi.Add(Crypto.EncryptTripleDES(Word, Key))
+                    Console.WriteLine(Word & " | " & Crypto.EncryptTripleDES(Word, Key) & " | " & Crypto.DecryptTripleDES(stfi(i), Key))
+                    i = i + 1
+                Next
+                Using str As FileStream = File.Create("filter.db")
+                    bf.Serialize(str, stfi)
+                    'Console.ReadLine()
+                End Using
+            End While
+        End Using
+
+        Console.WriteLine("--- DONE ---")
+        Console.WriteLine(Key)
+        Console.WriteLine(Crypto.ScrambleKey(Key))
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ FilterMD5/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FilterMD5")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("The MeaMod Group")> 
+<Assembly: AssemblyProduct("FilterMD5")> 
+<Assembly: AssemblyCopyright("Copyright © 2010 The MeaMod Group")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("34e02f08-45c8-4465-a99c-4ce31f595915")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
